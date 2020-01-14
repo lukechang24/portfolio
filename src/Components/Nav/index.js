268027f511b0792efe512336afb4cc9b9f60a975
lukechang24 <lukechang244@gmail.com>
2020-01-14 10:29:02 -0800

    add smooth scrolling

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -8,7 +8,7 @@ function Nav() {
                 <S.Name>Luke Chang</S.Name>
             </S.NameDiv>
             <S.NavLinkDiv>
-                <S.NavLink href="" target="_blank">About</S.NavLink>
+                <S.NavLink href="/#about">About</S.NavLink>
                 <S.NavLink href="https://www.linkedin.com/in/lukechang420108/" target="_blank">LinkedIn</S.NavLink>
                 <S.NavLink href="https://github.com/lukechang24" target="_blank">GitHub</S.NavLink>
             </S.NavLinkDiv>
