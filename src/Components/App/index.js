3f582691b2977561985b388b8c705dc4269777d0
lukechang24 <lukechang244@gmail.com>
2022-10-26 00:58:17 -0700

    finished home page

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,21 +1,18 @@
+import React from "react"
+import { Route, Routes } from "react-router-dom"
 
-function App() {
+import Navbar from "../Navbar"
+import Home from "../Home"
+import S from "./style"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <S.AppContainer className="App">
+      <Navbar />
+      <Routes>
+        <Route path="home" element={<Home />}></Route>
+      </Routes>
+    </S.AppContainer>
   );
 }
 
